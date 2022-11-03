b26daafaf69e2d8ba8c2e99c95c99d32f06d3d8c
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-11-04 01:11:37 +0300

    Create projects.jsx

---
+++ views/projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <>
+      <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css"></link>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+		<link rel="stylesheet" href="/public/styles.css" />
+      <script src="/public/script.js"></script>
+      <div className="App">
+        <header className="App-header">
+          
+        </header>
+      </div>
+    </>;
+  }
+}
+
+export default App;
